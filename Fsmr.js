6c007a2491c4fd98c491014258f13f6dfaaff078
Joshua Brown <jbrown@ellation.com>
2017-10-29 01:23:42 -0700

    improved setState function, but WIP

--- Fsmr.js
+++ Fsmr.js
@@ -49,12 +49,30 @@
       }
       return currentState;
     };
-    self.stateChange = function(newStateName) {
+    self.setState = function(newStateName) {
+      var newState = self.states.filter(function findState(state) {
+        return state.name === newStateName;
+      });
+      if (newState.length < 1) {
+        console.error('failure, ' + newStateName + ' is not a state.  Try', self.getStates());
+        return null;
+      }
       if (self.currentState === null && newStateName === self.entryState.name) { // set initial current state
         self.currentState = self.entryState;
-      } else if() {
-
+        console.log('success! entered state machine at state', self.currentState.name);
+        return self.currentState.name;
+      } else if (self.currentState === null && newStateName !== self.entryState.name) {
+        console.error('Failure, ' + newStateName + ' is not entry state.  Try to set state ' + self.entryState.name);
+        return null;
       }
+      if (self.currentState.canCall.indexOf(newStateName) !== -1) {
+        self.currenState = newState;
+        // todo: repair this. for some reason not setting current state here, !!!!!!!!!!!!!!!!!!!!!!!
+        console.log('success!', newState, self.currentState);
+      } else {
+        console.error('failure, ' + self.currentState.name + ' cannot move to ' + newStateName + ' state');
+      }
+      return self.currentState.name;
     };
 
     if (typeof stateName === 'string') { // make the first state, make it the entry state
@@ -66,12 +84,13 @@
     var self = this;
     var fsm = new FSM(stateName, callees, callers);
     console.log('fsm is', fsm);
-    this.machine = fsm;  // todo remove me!  just for dev!!!!!!!!!!!!!!!!!!!!!!!!!!
-    this.init = fsm.init;
-    this.addState = fsm.addState;
-    this.removeState = fsm.removeState;
-    this.states = fsm.getStates;
-    this.state = fsm.state;
+    self.machine = fsm;  // todo remove me!  just for dev!!!!!!!!!!!!!!!!!!!!!!!!!!
+    self.init = fsm.init;
+    self.addState = fsm.addState;
+    self.removeState = fsm.removeState;
+    self.states = fsm.getStates;
+    self.state = fsm.state;
+    self.setState = fsm.setState;
   };
 
   global.Fsmr = global.F$ = Fsmr;
